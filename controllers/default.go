d4485903262944513d1e8eee4970009025a3437e
Wei Yang <richard.weiyang@gmail.com>
2017-11-01 07:56:45 +0800

    beego template examples

--- controllers/default.go
+++ controllers/default.go
@@ -11,4 +11,23 @@ type MainController struct {
 func (c *MainController) Get() {
 	c.TplName = "index.tpl"
 	beego.Trace("test trace")
+
+	c.Data["TrueCond"] = true
+	c.Data["FalseCond"] = false
+
+	type u struct {
+		Name string
+		Age  int
+		Sex  string
+	}
+
+	user := &u{
+		Name: "Joe",
+		Age:  20,
+		Sex:  "Male",
+	}
+	c.Data["User"] = user
+
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+	c.Data["Nums"] = &nums
 }
--- views/index.tpl
+++ views/index.tpl
@@ -3,5 +3,20 @@
 {{define "body"}}
 <div>
 This is a test ???
+<div>
+</div>
+{{if .TrueCond}}
+true Condition
+{{end}}
+<div>
+</div>
+{{with .User}}
+{{.Name}}; {{.Age}}; {{.Sex}}
+{{end}}
+</div>
+<div>
+{{range .Nums}}
+{{.}}
+{{end}}
 </div>
 {{end}}
