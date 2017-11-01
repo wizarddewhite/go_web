b0664e875259927c60710fb88657f1425f4eb301
Wei Yang <richard.weiyang@gmail.com>
2017-11-01 09:51:02 +0800

    create Category and Topic table

--- main.go
+++ main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	"web/models"
 	_ "web/routers"
 )
 
+func init() {
+	models.RegisterDB()
+}
+
 func main() {
+	orm.Debug = true
+	orm.RunSyncdb("default", false, true)
 	beego.Run()
 }
