fd452ab30b81c374b1568323106573233c57a3ee
Wei Yang <richard.weiyang@gmail.com>
2017-11-03 16:37:02 +0800

    category view and database

---
+++ controllers/category.go
@@ -0,0 +1,55 @@
+package controllers
+
+import (
+	//	"fmt"
+
+	"github.com/astaxie/beego"
+
+	"web/models"
+)
+
+type CategoryController struct {
+	beego.Controller
+}
+
+func (this *CategoryController) Get() {
+	op := this.Input().Get("op")
+
+	switch op {
+	case "add":
+		name := this.Input().Get("name")
+		if len(name) == 0 {
+			break
+		}
+
+		beego.Trace("category/add")
+		err := models.AddCategory(name)
+		if err != nil {
+			beego.Error(err)
+		}
+		this.Redirect("/category", 301)
+		return
+	case "del":
+		id := this.Input().Get("id")
+		if len(id) == 0 {
+			break
+		}
+
+		err := models.DeleteCategory(id)
+		if err != nil {
+			beego.Error(err)
+		}
+		this.Redirect("/category", 301)
+		return
+	}
+
+	this.TplName = "category.html"
+	this.Data["Title"] = "Category"
+	this.Data["IsCategory"] = true
+
+	var err error
+	this.Data["Categories"], err = models.GetAllCategories()
+	if err != nil {
+		beego.Error(err)
+	}
+}
--- models/models.go
+++ models/models.go
@@ -3,6 +3,7 @@ package models
 import (
 	"os"
 	"path"
+	"strconv"
 	"time"
 
 	"github.com/Unknwon/com"
@@ -23,6 +24,49 @@ type Category struct {
 	TopicTime       time.Time
 	TopicCount      int64
 	TopicLastUserId int64
+}
+
+func AddCategory(name string) error {
+	o := orm.NewOrm()
+
+	cate := &Category{
+		Title:     name,
+		Created:   time.Now(),
+		TopicTime: time.Now(),
+	}
+
+	qs := o.QueryTable("category")
+	err := qs.Filter("title", name).One(cate)
+	if err == nil {
+		return err
+	}
+
+	_, err = o.Insert(cate)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteCategory(id string) error {
+	cid, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		return err
+	}
+
+	o := orm.NewOrm()
+	cate := &Category{Id: cid}
+	_, err = o.Delete(cate)
+	return err
+}
+
+func GetAllCategories() ([]*Category, error) {
+	o := orm.NewOrm()
+
+	cates := make([]*Category, 0)
+	qs := o.QueryTable("category")
+	_, err := qs.All(&cates)
+	return cates, err
 }
 
 type Topic struct {
