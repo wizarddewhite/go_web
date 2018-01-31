61684abeb35cd9d5d8988aa3bc621243e7d1f71a
Wei Yang <richard.weiyang@gmail.com>
2018-01-31 12:02:17 +0800

    hide category

--- routers/router.go
+++ routers/router.go
@@ -9,12 +9,12 @@ import (
 func init() {
 	beego.Router("/", &controllers.MainController{})
 	beego.Router("/login", &controllers.LoginController{})
-	beego.Router("/category", &controllers.CategoryController{})
-	beego.Router("/topic", &controllers.TopicController{})
-	beego.AutoRouter(&controllers.TopicController{})
-	beego.Router("/reply", &controllers.ReplyController{})
-	beego.Router("/reply/add", &controllers.ReplyController{}, "post:Add")
-	beego.Router("/reply/delete", &controllers.ReplyController{}, "get:Delete")
+	//beego.Router("/category", &controllers.CategoryController{})
+	//beego.Router("/topic", &controllers.TopicController{})
+	//beego.AutoRouter(&controllers.TopicController{})
+	//beego.Router("/reply", &controllers.ReplyController{})
+	//beego.Router("/reply/add", &controllers.ReplyController{}, "post:Add")
+	//beego.Router("/reply/delete", &controllers.ReplyController{}, "get:Delete")
 
 	beego.Router("/statistic/update", &controllers.StatisticController{}, "post:Update")
 	beego.Router("/account", &controllers.AccountController{})
